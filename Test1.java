560f73337965bf6857c31f4039083d70603239ad
shyam <shyam07@gmail.com>
2023-10-13 15:55:50 +0530

    commited test1 file in git

---
+++ Test1.java
@@ -0,0 +1,25 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+	
+List<String> fruits = new ArrayList<>();
+
+fruits.add("Orang");
+fruits.add("Banana");
+fruits.add("kiwi");
+fruits.add("Apple");
+
+System.out.println(fruits);
+ for(String aa:fruits) {
+	 System.out.println(aa);
+ }
+
+	}
+
+}
