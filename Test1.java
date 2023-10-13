de3f72ea7588ba932ad6e56d39fe0e1232ed8b02
shyam <shyam07@gmail.com>
2023-10-13 16:41:37 +0530

    commited modified file

--- Test1.java
+++ Test1.java
@@ -20,6 +20,27 @@ System.out.println(fruits);
 	 System.out.println(aa);
  }
 
+ List<String> fruits1 = new ArrayList<>();
+
+ fruits1.add("Banana");
+ fruits1.add("Apple");
+ 
+ fruits1.addAll(fruits);
+ System.out.println(fruits1);
+ System.out.println("----------------------------");
+
+System.out.println(fruits1.size());
+System.out.println(fruits1.contains("Banana"));
+System.out.println(fruits1.indexOf("Banana"));
+System.out.println(fruits1.get(1));
+System.out.println(fruits1.set(1, "dragon"));
+ System.out.println(fruits1);
+ System.out.println(fruits1.remove(1));
+ System.out.println(fruits1);
+ fruits1.removeAll(fruits1);
+ System.out.println("----------------------------");
+System.out.println(fruits1);
+
 	}
 
 }
